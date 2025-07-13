1b017bcabb946c4c31e1310d593f57584374dfe1
harold-hurst <harry.hurst2636@gmail.com>
2025-07-13 14:11:24 +0100

    add contact form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import EmailForm from "../components/EmailForm";
 
 import { useEffect, useState } from "react";
 
@@ -739,6 +740,21 @@ export default function Home() {
                   </span>
                 </a>
               </div>
+            </div>
+          </section>
+
+          <section
+            id="contact"
+            className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+            aria-label="Contact me"
+          >
+            <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-cyan-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+              <h2 className="text-sm font-bold uppercase tracking-widest lg:sr-only">
+                Contact
+              </h2>
+            </div>
+            <div>
+              <EmailForm />
             </div>
           </section>
 
---
+++ src/components/EmailForm.js
@@ -0,0 +1,66 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { sendEmail } from '@/utils/send-email';
+
+const EmailForm = () => {
+  const { register, handleSubmit } = useForm();
+
+  function onSubmit(data) {
+    sendEmail(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className='mb-5'>
+        <label
+          htmlFor='name'
+          className='mb-3 block text-base font-medium'
+        >
+          Full Name
+        </label>
+        <input
+          type='text'
+          placeholder='Full Name'
+          className='w-full rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-cyan-400 focus:shadow-md'
+          {...register('name', { required: true })}
+        />
+      </div>
+      <div className='mb-5'>
+        <label
+          htmlFor='email'
+          className='mb-3 block text-base font-medium'
+        >
+          Email Address
+        </label>
+        <input
+          type='email'
+          placeholder='example@domain.com'
+          className='w-full rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-cyan-400 focus:shadow-md'
+          {...register('email', { required: true })}
+        />
+      </div>
+      <div className='mb-5'>
+        <label
+          htmlFor='message'
+          className='mb-3 block text-base font-medium'
+        >
+          Message
+        </label>
+        <textarea
+          rows={4}
+          placeholder='Type your message'
+          className='w-full resize-none rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-cyan-400 focus:shadow-md'
+          {...register('message', { required: true })}
+        ></textarea>
+      </div>
+      <div>
+        <button className='hover:shadow-form rounded-md bg-cyan-400 py-3 px-8 text-base font-semibold text-white outline-none'>
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default EmailForm;
