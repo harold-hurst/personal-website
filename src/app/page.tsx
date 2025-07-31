10d24b851a96e1b7122e7a27ae2e0937efcc3bed
harold-hurst <harry.hurst2636@gmail.com>
2025-07-31 15:03:18 +0100

    add pizza shop website to projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -475,7 +475,7 @@ export default function Home() {
                         href="https://jamming-peach.vercel.app/"
                         target="_blank"
                         rel="noreferrer noopener"
-                        aria-label="Jammming Spotify Web App (opens in a new tab)"
+                        aria-label="Pizza shop website (opens in a new tab)"
                       >
                         <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
                         <span>
@@ -534,6 +534,74 @@ export default function Home() {
                     data-nimg="1"
                     className="aspect-video object-cover rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
                     src="/jamming.png"
+                  />
+                </div>
+              </li>
+
+              <li className="mb-12 cursor-pointer">
+                <div className="group relative grid gap-4 pb-1 sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                  <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition-all duration-300 motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                  <div className="z-10 sm:order-2 sm:col-span-6">
+                    <h3>
+                      <a
+                        className="inline-flex items-baseline font-medium leading-tight hover:text-cyan-400 focus-visible:text-cyan-400  group/link text-base"
+                        href="https://izza-umber.vercel.app/"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        aria-label="Pizza Shop Website (opens in a new tab)"
+                      >
+                        <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                        <span>
+                          Pizza Shop Website
+                          <span className="inline-block">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                              className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                              aria-hidden="true"
+                            >
+                              <path
+                                fillRule="evenodd"
+                                d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                clipRule="evenodd"
+                              ></path>
+                            </svg>
+                          </span>
+                        </span>
+                      </a>
+                    </h3>
+                    <p className="mt-2 text-sm leading-normal">
+                      AUthentic Italian pizza shop website, showcasing a
+                      selection of pizzas, with a focus on responsive design and
+                      user experience.
+                    </p>
+                    <ul
+                      className="mt-2 flex flex-wrap"
+                      aria-label="Technologies used:"
+                    >
+                      <li className="mr-1.5 mt-2">
+                        <div className="border border-cyan-400 transition-all duration-300 px-3 rounded py-1 text-xs font-medium leading-5  ">
+                          Next JS
+                        </div>
+                      </li>
+                      <li className="mr-1.5 mt-2">
+                        <div className="border border-cyan-400 transition-all duration-300 px-3 rounded py-1 text-xs font-medium leading-5  ">
+                          Tailwind CSS
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+
+                  <Image
+                    alt="Pizza Shop Website"
+                    loading="lazy"
+                    width="200"
+                    height="48"
+                    decoding="async"
+                    data-nimg="1"
+                    className="aspect-video object-cover rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+                    src="/izza.png"
                   />
                 </div>
               </li>
